ea3fab42eea842258b2f92e597128e1df4f37f3f
Doli282 <DolanskyL@email.cz>
2022-11-07 21:01:59 +0100

    ag1: hw1 has AVLTree with insert

---
+++ hw2/hw2.cpp
@@ -0,0 +1,658 @@
+#ifndef __PROGTEST__
+#include <cassert>
+#include <iostream>
+#include <memory>
+#include <limits>
+#include <optional>
+#include <algorithm>
+#include <bitset>
+#include <list>
+#include <array>
+#include <vector>
+#include <deque>
+#include <unordered_set>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <random>
+
+#endif
+
+// TODO implement
+template <typename Product>
+struct Bestsellers
+{
+public:
+  // The total number of tracked products
+  size_t products() const
+  {
+    return productsNO;
+  }
+
+  void sell(const Product &p, size_t amount)
+  {
+    std::shared_ptr<Node> newProduct = std::make_shared<Node>(amount, p); // new possible Product
+    if (productsDB.emplace(p, newProduct).second)                      // if product insert successful -> was not in the table
+    {
+      productsNO++;
+      addToTree(newProduct, amount);
+    }
+    else
+    {
+      addSale(p, amount);
+    }
+    return;
+  }
+
+  // The most sold product has rank 1
+  size_t rank(const Product &p) const;
+  const Product &product(size_t rank) const;
+
+  // How many copies of product with given rank were sold
+  size_t sold(size_t rank) const;
+  // The same but sum over interval of products (including from and to)
+  // It must hold: sold(x) == sold(x, x)
+  size_t sold(size_t from, size_t to) const;
+
+  // Bonus only, ignore if you are not interested in bonus
+  // The smallest (resp. largest) rank with sold(rank) == sold(r)
+  size_t first_same(size_t r) const { return 0; }
+  size_t last_same(size_t r) const { return 0; }
+
+  void showTree()
+  {
+    std::cout << "=== TREE ===" << std::endl;
+    // treeShow(root, "", false);
+    treeShow(root, -1);
+    std::cout << "=== TREE END ===" << std::endl;
+    DBShow();
+    std::cout << std::endl;
+    return;
+  }
+
+private:
+  struct Node
+  {
+    Node(size_t amount, Product productName) : amountSold(amount), productID(productName) {}
+    int delta = 0;
+    u_int32_t rank;
+    size_t amountSold = 0;
+    std::weak_ptr<Node> parent;
+    std::shared_ptr<Node> rightNode;
+    std::shared_ptr<Node> leftNode;
+    std::shared_ptr<Node> followingNode;
+    Product productID;
+  };
+
+  std::shared_ptr<Node> root = nullptr;
+  size_t productsNO = 0;
+  std::unordered_map<Product, std::shared_ptr<Node>> productsDB;
+
+  void setChild(std::shared_ptr<Node> adoptiveParent, std::shared_ptr<Node> child, bool isLeftChild)
+  {
+    if (adoptiveParent == nullptr) // parent does not exist -> it was root
+    {
+      root = child;
+    }
+    else
+    {
+      if (isLeftChild) // set child as left or right accordingly
+      {
+        adoptiveParent->leftNode = child;
+      }
+      else
+      {
+        adoptiveParent->rightNode = child;
+      }
+    }
+    if(child)
+    {
+      child->parent = adoptiveParent; // child has new parent
+    }
+  }
+
+  void rotateRight(std::shared_ptr<Node> x)
+  {
+    std::shared_ptr<Node> y = x->leftNode;
+    std::shared_ptr<Node> treeA = y->leftNode;
+    std::shared_ptr<Node> treeB = y->rightNode;
+    std::shared_ptr<Node> treeC = x->rightNode;
+    std::shared_ptr<Node> parent = x->parent.lock();
+    setChild(x, treeB, true);
+    setChild(y, x, false);
+    if(parent)
+    {
+      setChild(parent, y, (x == parent->leftNode));
+    }
+    else
+    {
+      root = y;
+      y->parent = std::weak_ptr<Node>();
+    }
+    x->delta = (++(y->delta)) ? -1 : 0; // alter delta accordingly to(from) Y=+1(0) && X=-1(-2) || Y=0(-1) && X=0(-2)
+  }
+  void rotateLeft(std::shared_ptr<Node> x)
+  {
+    std::shared_ptr<Node> y = x->rightNode;
+    std::shared_ptr<Node> treeB = y->leftNode;
+    std::shared_ptr<Node> parent = x->parent.lock();
+    setChild(x, treeB, false);
+    setChild(y, x, true);
+    if(parent)
+    {
+      setChild(parent, y, (x == parent->leftNode));
+    }
+    else
+    {
+      root = y;
+      y->parent = std::weak_ptr<Node>();
+    }
+    x->delta = (--(y->delta)) ? +1 : 0; // alter delta accordingly to(from) Y=-1(0) && X=+1(+2) || Y=0(+1) && X=0(+2)
+  }
+  void rotateRightLeft(std::shared_ptr<Node> x) // double rotation to the left -> first to the right, then to the left
+  {
+    std::shared_ptr<Node> y = x->rightNode;
+    std::shared_ptr<Node> z = y->leftNode;
+    std::shared_ptr<Node> treeA = x->leftNode;
+    std::shared_ptr<Node> treeB = z->leftNode;
+    std::shared_ptr<Node> treeC = z->rightNode;
+    std::shared_ptr<Node> treeD = y->rightNode;
+    std::shared_ptr<Node> parent = x->parent.lock();
+    
+    setChild(x, treeB, false);
+    setChild(y, treeC, true);
+    setChild(z, x, true);
+    setChild(z, y, false);
+    if(parent)
+    {
+      setChild(parent, z, (x == parent->leftNode));
+    }
+    else
+    {
+      root = z;
+      z->parent = std::weak_ptr<Node>();
+    }
+
+    if(z->delta == +1)
+    {
+      x->delta = -1;
+      y->delta = 0;
+    }
+    else if(z->delta == -1)
+    {
+      x->delta = 0;
+      y->delta = +1;
+    }
+    else
+    {
+      x->delta = 0;
+      y->delta = 0;
+    }
+    z->delta = 0;
+  }
+  void rotateLeftRight(std::shared_ptr<Node> x) // double rotation to the right -> first to the left, then to the right
+  {
+    std::shared_ptr<Node> y = x->leftNode;
+    std::shared_ptr<Node> z = y->rightNode;
+    std::shared_ptr<Node> treeA = y->leftNode;
+    std::shared_ptr<Node> treeB = z->leftNode;
+    std::shared_ptr<Node> treeC = z->rightNode;
+    std::shared_ptr<Node> treeD = x->rightNode;
+    std::shared_ptr<Node> parent = x->parent.lock();
+
+    setChild(x, treeC, true);
+    setChild(y, treeB, false);
+    setChild(z, x, false);
+    setChild(z, y, true);
+    if(parent)
+    {
+      setChild(parent, z, (x == parent->leftNode));
+    }
+    else
+    {
+      root = z;
+      z->parent = std::weak_ptr<Node>();
+    }
+    
+    if(z->delta == +1)
+    {
+      x->delta = 0;
+      y->delta = -1;
+    }
+    else if(y->delta == -1)
+    {
+      x->delta = +1;
+      y->delta = 0;
+    }
+    else
+    {
+      x->delta = 0;
+      y->delta = 0;
+    }
+    z->delta = 0;
+  }
+
+  void balanceTreeAfterInsert(std::shared_ptr<Node> parent, std::shared_ptr<Node> child)
+  {
+    int balanceFactor;
+    while (parent) // propagate upstream up to the root
+    {
+      balanceFactor = parent->delta += (child == parent->leftNode) ? (-1) : 1; // -1 for left child, +1 for right child
+
+      if (balanceFactor < -1) // tree is left-heavy
+      {
+        if (child->delta > 0) // information came from the inside node
+        {
+          //std::cout << "rotuji vlevo a vpravo" << std::endl;
+          rotateLeftRight(parent);
+        }
+        else
+        {
+          //std::cout << "rotuji vpravo" << std::endl;
+          rotateRight(parent);
+        }
+        return;
+      }
+      else if (balanceFactor > 1) // tree is right-heavy
+      {
+        if (child->delta < 0) // information came from the inside node
+        {
+          //std::cout << "rotuji vpravo a vlevo" << std::endl;
+          rotateRightLeft(parent);
+        }
+        else
+        {
+          //std::cout << "rotuji vlevo" << std::endl;
+          rotateLeft(parent);
+        }
+        return;
+      }
+      else if (balanceFactor == 0)
+      { // tree got balanced, stop propagation
+        //std::cout << "koncim bez rotace" << std::endl;
+        return;
+      }
+
+      // move to the upper floor
+      //std::cout << "posouvam se vzhuru" << std::endl;
+      child = parent;
+      parent = parent->parent.lock();
+    }
+    return;
+  }
+
+  /* void balanceTreeAfterDelete(std::shared_ptr<Node> moved)
+  {
+    std::shared_ptr<Node> x = moved;
+    int BF;
+
+    while (x)
+    {
+      x->height = std::max(getHeight(x->leftNode), getHeight(x->rightNode)) + 1;
+      BF = balanceFactor(x);
+
+      if (BF == -1 || BF == 1)
+        return; // the height of the tree has not changed
+      else if (BF == 0)
+        ;              // tree got ideally balanced after the delete, but height decreased -> propagate further
+      else if (BF > 1) // tree got disbalanced
+      {                // tree is right-heavy
+        std::shared_ptr<Node> y = x->rightNode;
+        int BFY = balanceFactor(y);
+        if (BFY == 1) // rotate left and propagate
+        {
+          rotateLeft(x);
+        }
+        else if (BFY == 0) // rotate left and stop
+        {
+          rotateLeft(x);
+          break;
+        }
+        else // rotate right, rotate left and propagate
+        {
+          rotateRight(y);
+          rotateLeft(x);
+        }
+      }
+      else // (BFY < -1)
+      {    // tree is left-heavy
+        std::shared_ptr<Node> y = x->leftNode;
+        int BFY = balanceFactor(y);
+        if (BFY == -1) // rotate left and propagate
+        {
+          rotateRight(x);
+        }
+        else if (BFY == 0) // rotate left and stop
+        {
+          rotateRight(x);
+          break;
+        }
+        else // rotate right, rotate left and propagate
+        {
+          rotateLeft(y);
+          rotateRight(x);
+        }
+      }
+
+      x = x->parent.lock(); // propagate the change to upper levels
+    }
+    return;
+  } */
+
+  bool addToTree(std::shared_ptr<Node> child, const size_t amount)
+  {
+    if (!root)
+    { // check if the root is not nullptr
+      root = child;
+      return true;
+    }
+    std::shared_ptr<Node> parent = nullptr;
+    std::shared_ptr<Node> descendant = root;
+    while (descendant)
+    { // while I am not in a list
+      parent = descendant;
+      if (descendant->amountSold < amount)
+      { // if the key of a parent is less than the key of the child
+        descendant = descendant->rightNode;
+      }
+      else
+      {
+        descendant = descendant->leftNode;
+      }
+    }
+    if (parent->amountSold < amount)
+    { // if the key of a parent is less than the key of the child
+      parent->rightNode = child;
+    }
+    else
+    {
+      parent->leftNode = child;
+    }
+    child->parent = parent;
+    // propagace zmeny vysky stromu
+    balanceTreeAfterInsert(parent, child);
+    return true;
+  }
+
+  // bypass the removed node and connect its relatives
+  void bypass(std::shared_ptr<Node> removed, std::shared_ptr<Node> child)
+  {
+    if (removed->parent.lock() == nullptr)                // removed node is root
+      root = child;                                       // set new root
+    else if (removed == removed->parent.lock()->leftNode) // removed node is left child
+      removed->parent.lock()->leftNode = child;           // grandparent is now connected to the grandchild
+    else                                                  // removed node is right child
+      removed->parent.lock()->rightNode = child;          // grandparent is now connected to the grandchild
+
+    if (child)                         // change the upgoing inheritance (if child exists)
+      child->parent = removed->parent; // child's parent is now its grandparent
+  }
+
+  std::shared_ptr<Node> findSuccessor(std::shared_ptr<Node> node)
+  {
+    while (node->leftNode)
+    {
+      node = node->leftNode;
+    }
+    return node;
+  }
+
+  /* bool deleteFromTree(std::shared_ptr<Node> changed)
+  {
+    std::shared_ptr<Node> disbalanced;
+    if (!changed->leftNode)
+    { // changed node does not have left child
+      disbalanced = ((changed->rightNode) ? (changed->rightNode) : (changed->parent.lock()));
+      bypass(changed, changed->rightNode);
+      balanceTreeAfterDelete(disbalanced); // balance tree after deletion
+    }
+    else if (!changed->rightNode)
+    { // changed node does not have right child
+      disbalanced = ((changed->leftNode) ? (changed->leftNode) : (changed->parent.lock()));
+      bypass(changed, changed->leftNode);
+      balanceTreeAfterDelete(disbalanced); // balance tree after delete
+    }
+    else
+    {                                                                      // changed node has both children
+      std::shared_ptr<Node> successor = findSuccessor(changed->rightNode); // find the element with th next key
+      if (successor->parent.lock() != changed)                             // if successor is not directly connected to changed node
+      {
+        disbalanced = successor->parent.lock();    // the first disbalanced node is parent of the successor
+        bypass(successor, successor->rightNode);   // bypass successor and reconnect its relatives
+        successor->rightNode = changed->rightNode; // successor is now parent of changed node's child
+        successor->rightNode->parent = successor;
+      }
+      else
+      { // if the changed node is the parent of the successor, then the first disbalanced node is successor himself
+        disbalanced = successor;
+      }
+      bypass(changed, successor);              // place successor on the position of the changed node
+      successor->leftNode = changed->leftNode; // connect successor with the second, left child of the changed node
+      successor->leftNode->parent = successor;
+      balanceTreeAfterDelete(disbalanced); // balance tree after delete
+      getHeight(successor);
+    }
+    return true;
+  } */
+
+  // add sale
+  void addSale(const Product &p, size_t amount)
+  {
+    std::shared_ptr<Node> changed = productsDB.at(p);
+    changed->amountSold += amount;
+    //deleteFromTree(changed);
+    // addToTree(changed, changed->amountSold);
+    return;
+  }
+
+  // print elements of a tree
+  void treeShow(std::shared_ptr<Node> node, const std::string &prefix, bool left)
+  {
+    if (node)
+    {
+      std::cout << prefix;
+      std::cout << (left ? "├─L─" : "└─R─");
+
+      std::cout << node->amountSold << std::endl;
+      treeShow(node->leftNode, prefix + (left ? "│   " : "    "), true);
+      treeShow(node->rightNode, prefix + (left ? "│   " : "    "), false);
+    }
+    return;
+  }
+  void treeShow(std::shared_ptr<Node> node, int prefix)
+  {
+    if(node)
+    {
+      prefix++;
+      treeShow(node->rightNode, prefix);
+      for (int i = 0; i < prefix; i++)
+        std::cout << "     ";
+      std::cout << node->amountSold << "[" << node->delta << "](" << (node->parent.lock() ? std::to_string(node->parent.lock()->amountSold) : "root") << ")" << std::endl;
+      treeShow(node->leftNode, prefix);
+    }
+    return;
+  }
+  void DBShow()
+  {
+    std::cout << "+++ Product DB +++" << std::endl;
+    for (auto &it : productsDB)
+    {
+      std::cout << it.first << " : " << it.second->amountSold << std::endl;
+    }
+    std::cout << "+++ +++ +++ +++ +++" << std::endl;
+  }
+};
+
+#ifndef __PROGTEST__
+
+void test1()
+{
+  std::cout << "TEST 1 -> prepared test" << std::endl;
+  Bestsellers<std::string> T;
+  T.sell("coke", 32);
+  T.sell("bread", 1);
+  assert(T.products() == 2);
+  T.sell("ham", 2);
+  T.sell("mushrooms", 12);
+
+  assert(T.products() == 4);
+  // assert(T.rank("ham") == 3);
+  // assert(T.rank("coke") == 1);
+  // assert(T.sold(1, 3) == 46);
+  // assert(T.product(2) == "mushrooms");
+
+  //T.sell("ham", 11);
+  // assert(T.products() == 4);
+  // assert(T.product(2) == "ham");
+  // assert(T.sold(2) == 13);
+  // assert(T.sold(2, 2) == 13);
+  // assert(T.sold(1, 2) == 45);
+  T.showTree();
+}
+
+void test2()
+{
+  /*
+  std::cout << "TEST 2 -> prepared test" << std::endl;
+    #define CATCH(expr)                 \
+    try                               \
+    {                                 \
+      expr;                           \
+      assert(0);                      \
+    }                                 \
+    catch (const std::out_of_range &) \
+    {                                 \
+      assert(1);                      \
+    };
+
+    Bestsellers<std::string> T;
+    T.sell("coke", 32);
+    T.sell("bread", 1);
+
+    CATCH(T.rank("ham"));
+    CATCH(T.product(3));
+    CATCH(T.sold(0));
+    CATCH(T.sold(9));
+    CATCH(T.sold(0, 1));
+    CATCH(T.sold(3, 2));
+    CATCH(T.sold(1, 9));
+
+  #undef CATCH */
+}
+
+void test0()
+{
+  std::cout << "TEST 0 -> basic test" << std::endl;
+  Bestsellers<std::string> T;
+  T.sell("coke", 12);
+  T.showTree();
+  T.sell("bread", 1);
+  assert(T.products() == 2);
+  T.showTree();
+  T.sell("ham", 2);
+  T.showTree();
+  T.sell("mushrooms", 32);
+  T.showTree();
+}
+void test3()
+{
+  std::cout << "TEST 3 -> increasing series" << std::endl;
+  Bestsellers<std::string> T;
+  T.sell("coke", 10);
+  T.showTree();
+  T.sell("bread", 20);
+  T.showTree();
+  assert(T.products() == 2);
+  T.sell("ham", 30);
+  T.showTree();
+  T.sell("mushrooms", 40);
+  T.showTree();
+  T.sell("a", 50);
+  T.showTree();
+  T.sell("b", 60);
+  T.sell("c", 70);
+  T.sell("d", 80);
+  T.showTree();
+}
+void test4()
+{
+  std::cout << "TEST 4 -> decreasing series" << std::endl;
+  Bestsellers<std::string> T;
+  T.sell("coke", 80);
+  T.sell("bread", 70);
+  assert(T.products() == 2);
+  T.sell("ham", 60);
+  T.sell("mushrooms", 50);
+  // T.showTree();
+  T.sell("a", 40);
+  T.sell("b", 30);
+  T.sell("c", 20);
+  T.sell("d", 10);
+  T.showTree();
+}
+void test5()
+{
+  std::cout << "TEST 5 -> same numbrers" << std::endl;
+  Bestsellers<std::string> T;
+  T.sell("coke", 80);
+  T.sell("bread", 70);
+  assert(T.products() == 2);
+  T.sell("ham", 60);
+  T.sell("mushrooms", 60);
+  // T.showTree();
+  T.sell("a", 40);
+  T.sell("b", 30);
+  T.sell("c", 40);
+  T.sell("d", 60);
+  T.showTree();
+}
+void test6()
+{
+  std::cout << "TEST 6 -> sell same products" << std::endl;
+  Bestsellers<std::string> T;
+  T.sell("coke", 80);
+  T.sell("bread", 70);
+  assert(T.products() == 2);
+  T.sell("ham", 60);
+  T.sell("mushrooms", 50);
+  // T.showTree();
+  T.sell("a", 40);
+  T.sell("b", 30);
+  T.sell("c", 20);
+  T.sell("d", 10);
+  T.showTree();
+  T.sell("a", 4);
+  T.showTree();
+  T.sell("bread", 7);
+  T.showTree();
+}
+void test7(int data)
+{
+  std::cout << "TEST 7 -> random selling" << std::endl;
+  Bestsellers<std::string> T;
+  for (int i = 0; i < data; i++)
+  {
+    T.sell("a" + std::to_string(i), rand() % (data * 2));
+  }
+  T.showTree();
+  for (int i = 0; i < 10; i++)
+  {
+    T.sell("a" + std::to_string(i), -100);
+    T.showTree();
+  }
+}
+
+int main()
+{
+  srand(time(0));
+  test0();
+  test1();
+  test2();
+  test3();
+  test4();
+  test5();
+  test6();
+  //test7(30);
+}
+
+#endif
