0dadd466c5302b3f83dfadae3b5b8ac9f4709f6e
Doli282 <DolanskyL@email.cz>
2022-11-11 23:48:42 +0100

    [aps] hw1: processor completed (memory access implemented as wires)

--- aps/hw1/Dolansky_Lukas_CPU.v
+++ aps/hw1/Dolansky_Lukas_CPU.v
@@ -2,34 +2,28 @@
 module processor( input         clk, reset,
                   output reg [31:0] PC,
                   input  [31:0] instruction,
-                  output reg       WE,
-                  output reg [31:0] address_to_mem,
-                  output reg [31:0] data_to_mem,
+                  output       WE,
+                  output [31:0] address_to_mem,
+                  output [31:0] data_to_mem,
                   input  [31:0] data_from_mem
                 );
     //... write your code here ...
 	
   reg signed [31:0] r [31:0]; // 32 bit registers (32 of them)
-  integer i;
   // Control Unit
+
+  assign WE = (instruction[6:0] == 'b0100011) ? (1) : 0;
+  assign data_to_mem = r[instruction[24:20]];
+  assign address_to_mem = (instruction[6:0] == 'b0100011) ? (r[instruction[19:15]] + {{20{instruction[31]}}, {instruction[31:25], instruction[11:7]}}) : (r[instruction[19:15]] + {{20{instruction[31]}}, instruction[31:20]});
+
   always@(posedge clk) begin
-    $display(" ");
-    //$display("===== CPU ha isogashi desu =======");
-    $display("PC before instruction = %h", PC);
-    //$display("Instruction = %b", instruction);
-    WE = 0;
     r[0] = 0;
     if(reset) begin
       PC = 0;
-      //$display("reset = %b, PC = %h", reset, PC);
     end
     else begin
-    //$display("Instruction[6:0] = %b", instruction[6:0]);
       case(instruction[6:0])
         'b0110011: // add, and, sub, sll, srl, sra
-          begin
-            //$display("Instruction[14:12] = %b, rd = %d, rs1 = %d, rs2 = %d", instruction[14:12],  instruction[11:7], instruction[19:15], instruction[24:20]);
-            //$display("BEFORE: rd = %d, rs1 = %d, rs2 = %d", r[instruction[11:7]], r[instruction[19:15]], r[instruction[24:20]]);
           case(instruction[14:12])
             'b000: // add, sub
               case(instruction[31:25])
@@ -53,86 +47,40 @@ module processor( input         clk, reset,
                 'b0000000: // srl: rd = (unsigned)rs1 >> rs2 (logical) 
                   r[instruction[11:7]] = r[instruction[19:15]] >> r[instruction[24:20]];
                 'b0100000: // sra: rd = (signed)rs1 >>rs2 (arithmetic)
-                  begin
-            //        $display("SRA: r%d = %d >>> %d", instruction[19:15], r[instruction[19:15]], r[instruction[24:20]]);
                     r[instruction[11:7]] = (r[instruction[19:15]] >>> r[instruction[24:20]]);
-                  end
               endcase
           endcase
-            //$display("AFTER: rd = %d, rs1 = %d, rs2 = %d", r[instruction[11:7]], r[instruction[19:15]], r[instruction[24:20]]);
-          end
         'b0010011: // addi: rd = rd1 + imm12
-          begin
-            //$display("rd = %d, rs1 = %d, imm = %d",  instruction[11:7], instruction[19:15], instruction[31:20]);
-            //$display("BEFORE: rd = %d, rs1 = %d", r[instruction[11:7]], r[instruction[19:15]]);
           r[instruction[11:7]] = r[instruction[19:15]] + {{20{instruction[31]}}, instruction[31:20]};
-            //$display("AFTER: rd = %d, rs1 = %d", r[instruction[11:7]], r[instruction[19:15]]);
-          end
         'b1100011: // beq, blt
-          begin
-            //$display("Instruction[14:12] = %b, rs%d = %d, rs%d = %d", instruction[14:12], instruction[19:15], r[instruction[19:15]], instruction[24:20], r[instruction[24:20]]);
           case(instruction[14:12])
             'b000: // beq: if [rs1] == [rs2] go to [PC]+{imm12:1,'0'}; else go to [PC]+4;	
               if(r[instruction[19:15]] == r[instruction[24:20]]) PC = PC + {{19{instruction[31]}}, instruction[31], instruction[7], instruction[30:25], instruction[11:8], 1'b0} - 4;
-              //else PC = PC + 4;
             'b001: //bne: if rs1 != rs2 go to PC+imm*2; else go to PC+4
               if(r[instruction[19:15]] != r[instruction[24:20]]) PC = PC + {{19{instruction[31]}},instruction[31], instruction[7], instruction[30:25], instruction[11:8], 1'b0} - 4;
-              //else PC = PC + 4;
             'b100: //blt: if [rs1] < [rs2] go to [PC]+{imm12:1,'0'}; else go to [PC]+4;	
               if(r[instruction[19:15]] < r[instruction[24:20]]) PC = PC + {{19{instruction[31]}}, instruction[31], instruction[7], instruction[30:25], instruction[11:8], 1'b0} - 4;
-              //else PC = PC + 4;
           endcase
-            //$display("PC = %d", PC);
-          end
-        'b0000011: // lw: rd ← Memory[[rs1] + imm11:0]
-          begin
-            $display("lw: r%d <- mem[r%d[%d] + imm[%d]",  instruction[11:7], instruction[19:15], r[instruction[19:15]], {{20{instruction[31]}}, instruction[31:20]});
-            address_to_mem = r[instruction[19:15]] + {{20{instruction[31]}}, instruction[31:20]};
-            r[0] = 0;
+         'b0000011: // lw: rd ← Memory[[rs1] + imm11:0]
             r[instruction[11:7]] = data_from_mem;
-            $display("lw: r%d <- %d",  instruction[11:7], data_from_mem);
-          end
-        'b0100011: // sw: Memory[[rs1] + imm11:0] ← [rs2];
-          begin
-            WE = 1;
-            address_to_mem = r[instruction[19:15]] + {{20{instruction[31]}}, {instruction[31:25], instruction[11:7]}};
-            data_to_mem = r[instruction[24:20]];
-            $display("sw: WE = %d, addr = %d, data = %d", WE, address_to_mem, data_to_mem);
-            $display("sw: r%d, %d(r%d)", instruction[24:20], {{20{instruction[31]}}, {instruction[31:25], instruction[11:7]}}, instruction[19:15]);
-          end
         'b0110111: // lui: rd ← {imm31:12,'0000 0000 0000'};
-          begin
-            //$display("lui ");
           r[instruction[11:7]] = {instruction[31:12], 12'b0000_0000_0000};
-          //$display("lui: ");
-          end
         'b1101111: // jal: rd ← [PC]+4; go to [PC] +{imm20:1,'0'};
           begin
-            //$display("jal r%d, label", instruction[11:7]);
             r[instruction[11:7]] = PC + 4;
             PC = PC + {{11{instruction[31]}}, instruction[31], instruction[19:12], instruction[20], instruction[30:21], 1'b0} - 4;
-            //$display("jal: PC = %d", PC);
           end
         'b1100111: // jalr: rd ← [PC]+4; go to [rs1]+imm11:0;
           begin
-            //$display("jalr r%d, label", instruction[11:7]);
             r[instruction[11:7]] = PC + 4;
             PC = r[instruction[19:15]] + {{20{instruction[31]}}, instruction[31:20]} - 4;
-            //$display("jalr: PC = %d", PC);
           end
         'b0010111: // auipc: rd ← [PC] + {imm31:12,'0000 0000 0000'};
-          begin
-            //$display("auipc: r%d = %d", instruction[11:7], PC + {instruction[31:12], 12'b0000_0000_0000});
           r[instruction[11:7]] = PC + {instruction[31:12], 12'b0000_0000_0000};
-          end
       endcase
       r[0] = 0; // verify that r0 is zero
-      // increment PC
-      PC = PC + 4;
+      PC = PC + 4; // increment PC
     end
-    for(i = 0; i < 32; i= i+1)
-      $display("r[%d] = %d", i, r[i]);
-    $display("PC after instruction = %h", PC);
   end
 endmodule
 
--- aps/hw1/testbench.v
+++ aps/hw1/testbench.v
@@ -6,12 +6,15 @@ module testbench();
 
 	top simulated_system (clk, reset, data_to_mem, address_to_mem, write_enable);
 
+	integer i;
 	initial	begin
 		$dumpfile("test");
 		$dumpvars;
 		reset<=1; # 2; reset<=0;
-		#100;
+		#2000;
 		$writememh ("memfile_data_after_simulation.hex",simulated_system.dmem.RAM,0,63);
+		$display("succesful test: %d", simulated_system.CPU.r[8]);
+		$display("success / fail: %d", simulated_system.CPU.r[9]);
 		$finish;
 	end
 
@@ -28,8 +31,12 @@ module top (	input         clk, reset,
 
 	wire [31:0] pc, instruction, data_from_mem;
 
-	inst_mem  imem(pc[7:2], instruction);
+//	inst_mem  imem(pc[7:2], instruction);
+	inst_mem  imem(pc[8:2], instruction);
 	data_mem  dmem(clk, write_enable, address_to_mem, data_to_mem, data_from_mem);
+	always@(posedge clk)
+	begin
+	end
 	processor CPU(clk, reset, pc, instruction, write_enable, address_to_mem, data_to_mem, data_from_mem);
 endmodule
 
@@ -38,10 +45,11 @@ module data_mem (input clk, we,
 		 input  [31:0] address, wd,
 		 output [31:0] rd);
 
-	reg [31:0] RAM[63:0];
+//	reg [31:0] RAM[63:0];
+	reg [31:0] RAM[127:0];
 
 	initial begin
-		$readmemh ("memfile_data.hex",RAM,0,63);
+		$readmemh ("memfile_data.hex",RAM,0,127);
 	end
 
 	assign rd=RAM[address[31:2]]; // word aligned
@@ -52,12 +60,15 @@ module data_mem (input clk, we,
 endmodule
 
 //-------------------------------------------------------------------
-module inst_mem (input  [5:0]  address,
+//module inst_mem (input  [5:0]  address,
+module inst_mem (input  [6:0]  address,
 		 output [31:0] rd);
 
-	reg [31:0] RAM[63:0];
+//	reg [31:0] RAM[63:0];
+	reg [31:0] RAM[127:0];
 	initial begin
-		$readmemh ("memfile_inst.hex",RAM,0,63);
+		$readmemh ("Dolansky_Lukas_prog1.hex",RAM,0,127);
+//		$readmemh ("memfile_inst.hex",RAM,0,127);
 	end
 	assign rd=RAM[address]; // word aligned
 endmodule
