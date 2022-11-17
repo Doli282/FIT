f5f3dc0cb4166e06e109f8ef222010c7fa3db4a7
Doli282 <DolanskyL@email.cz>
2022-11-17 22:12:53 +0100

    [aag] hw: add function to sort out equivalence classes in DFA

--- aag/sample.cpp
+++ aag/sample.cpp
@@ -135,6 +135,105 @@ void removeSuperfluous(DFA& automaton)
     return;
 }
 
+void equivalence(DFA& automaton)
+{
+    std::map<State,unsigned int> stateToClass; // map states to their equivalence classes
+    std::map<unsigned int, std::set<State>> classToStates; // map classes to set of states in the class
+    std::map<State, std::map<Symbol, unsigned int>> tableOfTransitions; // table of states and their transitions to equivalence classes
+    State NOclasses = 2; // number of classes after the iteration
+    State NOclassesPrev = 0; // number of classes before the iteration
+    
+    automaton.m_States.clear(); // erase the original states
+    automaton.m_States.emplace(0); // fill two initial classes
+    automaton.m_States.emplace(1);
+    
+    // first iteration -> divide to terminal and non-terminal states
+    for(State state : automaton.m_States)
+    {
+        if(automaton.m_FinalStates.find(state) != automaton.m_FinalStates.end())
+        {
+            stateToClass.emplace(state, 1);
+            classToStates[1].emplace(state);
+        }
+        else
+        {
+            stateToClass.emplace(state, 0);
+            classToStates[0].emplace(state);
+        }
+    }
+    
+    // create and check transition tables
+    while(NOclassesPrev != NOclasses)
+    {
+        NOclassesPrev = NOclasses;
+        tableOfTransitions.clear(); // wipe the previous table clean
+        for(auto transition : automaton.m_Transitions) // for every transition fill the table
+        {
+            tableOfTransitions[transition.first.first].emplace(transition.first.second, stateToClass.at(transition.second));
+        }
+        
+        for(unsigned int i = 0; i < NOclasses; i++) // for every equivalence class ...
+        {
+            std::vector<unsigned int> derivedClasses; // vector with classes that are derived from the current class
+            for(State state1 : classToStates.at(i)) // ... for every state in the class 
+            {
+                // .. check its transitions with every other state
+                for(State state2: classToStates.at(i))
+                {
+                    if(tableOfTransitions.at(state1) != tableOfTransitions.at(state2)) // if the transitions are different, move the state2 to different class
+                    {
+                        classToStates.at(i).erase(state2);
+                        for(unsigned int j : derivedClasses) // try to find out, if it does not fit in already created !derived! class
+                        {
+                            if(tableOfTransitions.at(state2) == tableOfTransitions.at(*(classToStates.at(j).begin()))) // if it belongs to already created class, put it there
+                            {
+                                stateToClass.at(state2) = j; // assign new class
+                                classToStates.at(j).emplace(state2); // move the state to different class
+                                goto jump;
+                            }
+                        }
+                        derivedClasses.push_back(NOclasses); // create new class
+                        stateToClass.at(state2) = NOclasses; // assign new class
+                        classToStates.at(NOclasses).emplace(state2); // move the state to different class
+                        automaton.m_States.emplace(NOclasses); // save this class to the automaton
+                        NOclasses++;
+jump:
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    // table is finished
+
+    // recombine transition table
+    automaton.m_Transitions.clear();
+    for(auto it = tableOfTransitions.begin(); it != tableOfTransitions.end(); ++it)
+    {
+        for(auto it2 = it->second.begin(); it2 != it->second.end(); ++it2)
+        {
+            automaton.m_Transitions.emplace(std::make_pair(stateToClass.at(it->first), it2->first), it2->second);
+        }
+    }
+
+    automaton.m_InitialState = stateToClass.at(automaton.m_InitialState); // change the initial state
+    // upadte final states
+    std::set<State> finalStates;
+    for(State state : automaton.m_FinalStates) // save all classes that contain final state
+    {
+        finalStates.emplace(stateToClass.at(state));
+    }
+    automaton.m_FinalStates = finalStates;
+    return;
+}
+
+void minimize(DFA& automaton)
+{
+    removeUnreachable(automaton);
+    removeSuperfluous(automaton);
+    equivalence(automaton);
+}
+
 #ifndef __PROGTEST__
 
 // You may need to update this function or the sample data if your state naming strategy differs.
