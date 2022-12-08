943d1bc7433215ff13cc6dfba70121f004c8e13e
Doli282 <DolanskyL@email.cz>
2022-12-08 18:32:00 +0100

    [aps] hw2: round without function, save histogram individually (15 pts.)

--- aps/hw2/Dolansky_Lukas_main.c
+++ aps/hw2/Dolansky_Lukas_main.c
@@ -4,17 +4,19 @@
 #include <math.h>
 #include <stdlib.h>
 
+#pragma GCC optimize ("Ofast,unroll-loops")
+
 // calculate histogram
-void calculateHistogram(unsigned int *histogram, double Y)
+/* void calculateHistogram(unsigned short *histogram, unsigned char Y)
 {
     //double Y = round(0.2126 * R + 0.7152 * G + 0.0722 * B); // vzorec na histogram
-    if (Y < 153.0)
+    if (Y < 153)
     {
-        if (Y < 51.0)
+        if (Y < 51)
         { // <0,50>
             histogram[0]++;
         }
-        else if (Y < 102.0)
+        else if (Y < 102)
         { // <51,101>
             histogram[1]++;
         }
@@ -23,7 +25,7 @@ void calculateHistogram(unsigned int *histogram, double Y)
             histogram[2]++;
         }
     }
-    else if (Y < 204.0)
+    else if (Y < 204)
     { // <153,203>
         histogram[3]++;
     }
@@ -32,6 +34,16 @@ void calculateHistogram(unsigned int *histogram, double Y)
         histogram[4]++;
     }
     return;
+} */
+
+unsigned short sum(unsigned short * histogram, unsigned int start, unsigned int end)
+{
+    unsigned short result = 0;
+    for(; start <= end; start++)
+    {
+        result += histogram[start];
+    }
+    return result;
 }
 
 /* for (unsigned int w = 0, w1 = 1, w2 = 2; w < width3; w += 3, w1 += 3, w2 += 3)
@@ -42,15 +54,14 @@ void calculateHistogram(unsigned int *histogram, double Y)
         calculateHistogram(histogram, row[w], row[w1], row[w2]);
     } */
 
-void printBorder(FILE *fd, unsigned int *histogram, unsigned char *row, unsigned int width3)
+void printBorder(FILE *fd, unsigned short *histogram, unsigned char *row, unsigned short width3)
 {
-    for (unsigned int w = 0; w < width3;)
+    for (unsigned short w = 0; w < width3;w+=3)
     {
-        fputc(row[w++], fd);
-        fputc(row[w++], fd);
-        fputc(row[w++], fd);
-        calculateHistogram(histogram, round(0.2126 * row[w - 3] + 0.7152 * row[w - 2] + 0.0722 * row[w - 1]));
+        //calculateHistogram(histogram, 0.5 + (0.2126 * row[w] + 0.7152 * row[w+1] + 0.0722 * row[w+2]));
+        histogram[(int)(0.5 + (0.2126 * row[w] + 0.7152 * row[w+1] + 0.0722 * row[w+2]))]++;
     }
+    fwrite((unsigned char *)row, sizeof(unsigned char), width3, fd);
     return;
 }
 
@@ -65,14 +76,15 @@ int main(int argc, char **argv)
     unsigned char * row2;
     unsigned char * row3;
     unsigned char * rowOut;
-    unsigned int * histogram = (unsigned int *)calloc(sizeof(unsigned int), 5);
+    unsigned short * histogram = (unsigned short *)calloc(sizeof(unsigned short), 256);
     
-    unsigned int focused;
-    unsigned int w;
-    unsigned int width3;
+    unsigned short focused;
+    unsigned short w;
+    unsigned short width;
+    unsigned short width3;
 
-    unsigned int height;
-    unsigned int h;
+    unsigned short height;
+    unsigned short h;
 
     size_t outtmp;
     
@@ -81,10 +93,11 @@ int main(int argc, char **argv)
 
     // read and write header
     fseek(source, 3, SEEK_CUR);
-    outtmp = fscanf(source, "%u\n%u\n255\n", &width3, &height);
-    fprintf(output, "P6\n%u\n%u\n255\n", width3, height);
+    outtmp = fscanf(source, "%hu\n%hu\n255\n", &width3, &height);
+    fprintf(output, "P6\n%hu\n%hu\n255\n", width3, height);
 
     width3 *= 3;
+    width = width3-3;
     row1 = (unsigned char *)malloc(width3 * sizeof(unsigned char));
     row2 = (unsigned char *)malloc(width3 * sizeof(unsigned char));
     row3 = (unsigned char *)malloc(width3 * sizeof(unsigned char));
@@ -111,30 +124,33 @@ int main(int argc, char **argv)
 //        fputc(row2[0], output);
 //        fputc(row2[1], output);
 //        fputc(row2[2], output);
-        calculateHistogram(histogram, round(0.2126 * (rowOut[0] = row2[0]) + 0.7152 * (rowOut[1] = row2[1]) + 0.0722 * (rowOut[2] = row2[2])));
+        //calculateHistogram(histogram, 0.5 + (0.2126 * (rowOut[0] = row2[0]) + 0.7152 * (rowOut[1] = row2[1]) + 0.0722 * (rowOut[2] = row2[2])));
+        histogram[(int)(0.5 + (0.2126 * (rowOut[0] = row2[0]) + 0.7152 * (rowOut[1] = row2[1]) + 0.0722 * (rowOut[2] = row2[2])))]++;
         // convolution table
         //  0 -1  0
         // -1  5 -1
         //  0 -1  0
         w = 3;
-        for (; w < (width3 - 3);)
+        for (; w < width;)
         {
-            focused = 5 * row2[w] - row2[w - 3] - row2[w + 3] - row1[w] - row3[w];
+            focused = - row2[w - 3] + 5 * row2[w]  - row2[w + 3] - row1[w] - row3[w];
             rowOut[w++] = (focused > 255) ? ((focused > 2000) ? 0 : 255) : focused;
             //fputc(rowOut[w++], output);
-            focused = 5 * row2[w] - row2[w - 3] - row2[w + 3] - row1[w] - row3[w];
+            focused = - row2[w - 3] + 5 * row2[w] - row2[w + 3] - row1[w] - row3[w];
             rowOut[w++] = (focused > 255) ? ((focused > 2000) ? 0 : 255) : focused;
             //fputc(rowOut[w++], output);
-            focused = 5 * row2[w] - row2[w - 3] - row2[w + 3] - row1[w] - row3[w];
+            focused = - row2[w - 3] + 5 * row2[w] - row2[w + 3] - row1[w] - row3[w];
             rowOut[w++] = (focused > 255) ? ((focused > 2000) ? 0 : 255) : focused;
             //fputc(rowOut[w++], output);
 
-            calculateHistogram(histogram, round(0.2126 * rowOut[w - 3] + 0.7152 * rowOut[w - 2] + 0.0722 * rowOut[w - 1]));
+            histogram[(int)(0.5 + (0.2126 * rowOut[w - 3] + 0.7152 * rowOut[w - 2] + 0.0722 * rowOut[w - 1]))]++;
+            //calculateHistogram(histogram, 0.5 + (0.2126 * rowOut[w - 3] + 0.7152 * rowOut[w - 2] + 0.0722 * rowOut[w - 1]));
         }
 //        fputc(row2[w++], output);
 //        fputc(row2[w++], output);
 //        fputc(row2[w], output);
-        calculateHistogram(histogram, round(0.2126 * (rowOut[w] = row2[w]) + 0.7152 * (rowOut[w+1] = row2[w+1]) + 0.0722 * (rowOut[w+2] = row2[w+2])));
+        //calculateHistogram(histogram, 0.5 + (0.2126 * (rowOut[w] = row2[w]) + 0.7152 * (rowOut[w+1] = row2[w+1]) + 0.0722 * (rowOut[w+2] = row2[w+2])));
+        histogram[(int)(0.5 + (0.2126 * (rowOut[w] = row2[w]) + 0.7152 * (rowOut[w+1] = row2[w+1]) + 0.0722 * (rowOut[w+2] = row2[w+2])))]++;
         fwrite((unsigned char *)rowOut, sizeof(unsigned char), width3, output);
 
         // reconnect pointers
@@ -159,7 +175,11 @@ int main(int argc, char **argv)
 
     // write histogram to output file
     FILE *histFile = fopen("output.txt", "w");
-    fprintf(histFile, "%u %u %u %u %u", histogram[0], histogram[1], histogram[2], histogram[3], histogram[4]);
+    fprintf(histFile, "%u %u %u %u %u", sum(histogram, 0, 50),
+    sum(histogram, 51, 101),
+    sum(histogram, 102, 152),
+    sum(histogram, 153, 203),
+    sum(histogram, 204, 255));
     fclose(histFile);
     free(histogram);
 
