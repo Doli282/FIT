4eb7bc92f6c0f4c55c04662606dfd23ce20e5437
Doli282 <DolanskyL@email.cz>
2022-12-09 17:07:01 +0100

    [ag1] hw3: treeProblem solved for groups of 1

--- ag1/hw3/sample.cpp
+++ ag1/hw3/sample.cpp
@@ -21,7 +21,8 @@
 
 using ChristmasTree = size_t;
 
-struct TreeProblem {
+struct TreeProblem
+{
   int max_group_size;
   std::vector<uint64_t> gifts;
   std::vector<std::pair<ChristmasTree, ChristmasTree>> connections;
@@ -29,8 +30,87 @@ struct TreeProblem {
 
 #endif
 
-uint64_t solve(const TreeProblem&) {
-  // TODO implement
+struct Vertex
+{
+  Vertex(uint64_t gifts, ChristmasTree number) : giftsVertexIncluded(gifts), ID(number) {}
+  uint64_t giftsVertexIncluded;
+  uint64_t giftsVertexExcluded = 0;
+  ChristmasTree ID;
+  ChristmasTree father = 0;
+  std::vector<ChristmasTree> sons;
+
+  uint64_t maxGifts()
+  {
+    return (giftsVertexExcluded < giftsVertexIncluded) ? giftsVertexIncluded : giftsVertexExcluded;
+  }
+};
+
+uint64_t solve(const TreeProblem &town)
+{
+  // structures, variables
+  ChristmasTree NOVertices = 0;
+  Vertex* vertex;
+  Vertex* vertexSon;
+  std::deque<Vertex*> stack;
+  std::queue<Vertex*> queue;
+  std::vector<Vertex> vertices;
+  
+
+  //std::cout << "counting vertices: ";
+  // counting vertices
+  for(uint64_t gift : town.gifts)
+  {
+    vertices.emplace_back(gift, NOVertices++);
+  }
+  //std::cout << NOVertices << std::endl;
+
+  //std::cout << "symetrization" << std::endl;
+  // symetrization of connections
+  for(const auto & connection : town.connections)
+  {
+    vertices.at(connection.first).sons.push_back(connection.second);
+    vertices.at(connection.second).sons.push_back(connection.first);
+  }
+
+  //std::cout << "create spanning-tree" << std::endl;
+  // BFS to create a spanning-tree
+  queue.push(&vertices.at(0));
+  stack.push_back(&vertices.at(0));
+  while(!queue.empty())
+  {
+    vertex = queue.front();
+    queue.pop();
+    for(ChristmasTree son : vertex->sons)
+    {
+      if(son != vertex->father)
+      {
+        vertexSon = &vertices.at(son);
+        vertexSon->father = vertex->ID;
+        stack.push_back(vertexSon);
+        queue.push(vertexSon);
+      }
+    }
+  }
+
+  // memoziation
+  //std::cout << "do memoziation" << std::endl;
+  while(!stack.empty())
+  {
+    vertex = stack.back();
+    stack.pop_back();
+    for(ChristmasTree son : vertex->sons)
+    {
+      if(son != vertex->father)
+      {
+        vertexSon = &vertices.at(son);
+        vertex->giftsVertexIncluded += vertexSon->giftsVertexExcluded;
+        vertex->giftsVertexExcluded += vertexSon->maxGifts();
+      }
+    }
+  }
+  
+  //std::cout << "return maxGifts of 0: " << vertices.at(0).maxGifts() << " from: " << vertices.at(0).giftsVertexExcluded << "/" << vertices.at(0).giftsVertexIncluded << std::endl;
+  return vertices.at(0).maxGifts();
 }
 
 #ifndef __PROGTEST__
@@ -38,55 +118,166 @@ uint64_t solve(const TreeProblem&) {
 using TestCase = std::pair<uint64_t, TreeProblem>;
 
 const std::vector<TestCase> BASIC_TESTS = {
-  { 3, { 1, { 1, 1, 1, 2 }, { {0,3}, {1,3}, {2,3} }}},
-  { 4, { 1, { 1, 1, 1, 4 }, { {0,3}, {1,3}, {2,3} }}},
-  { 57, { 1, {
-    17, 11, 5, 13, 8, 12, 7, 4, 2, 8,
-  }, {
-    {1, 4}, {6, 1}, {2, 1}, {3, 8}, {8, 0}, {6, 0}, {5, 6}, {7, 2}, {0, 9},
-  }}},
-  { 85, { 1, {
-    10, 16, 13, 4, 19, 8, 18, 17, 18, 19, 10,
-  }, {
-    {9, 7}, {9, 6}, {10, 4}, {4, 9}, {7, 1}, {0, 2}, {9, 2}, {3, 8}, {2, 3}, {5, 4},
-  }}},
-  { 79, { 1, {
-    8, 14, 11, 8, 1, 13, 9, 14, 15, 12, 1, 11,
-  }, {
-    {9, 1}, {1, 2}, {1, 4}, {5, 10}, {7, 8}, {3, 7}, {11, 3}, {11, 10}, {6, 8}, {0, 1}, {0, 3},
-  }}},
-  { 102, { 1, {
-    15, 10, 18, 18, 3, 4, 18, 12, 6, 19, 9, 19, 10,
-  }, {
-    {10, 2}, {11, 10}, {6, 3}, {10, 8}, {5, 3}, {11, 1}, {9, 5}, {0, 4}, {12, 3}, {9, 7}, {11, 9}, {4, 12},
-  }}},
-  { 93, { 1, {
-    1, 7, 6, 18, 15, 2, 14, 15, 18, 8, 15, 1, 5, 6,
-  }, {
-    {0, 13}, {6, 12}, {0, 12}, {7, 8}, {8, 3}, {12, 11}, {12, 1}, {10, 12}, {2, 6}, {6, 9}, {12, 7}, {0, 4}, {0, 5},
-  }}},
+    {3, {1, {1, 1, 1, 2}, {{0, 3}, {1, 3}, {2, 3}}}},
+    {4, {1, {1, 1, 1, 4}, {{0, 3}, {1, 3}, {2, 3}}}},
+    {57, {1, {
+                 17,
+                 11,
+                 5,
+                 13,
+                 8,
+                 12,
+                 7,
+                 4,
+                 2,
+                 8,
+             },
+          {
+              {1, 4},
+              {6, 1},
+              {2, 1},
+              {3, 8},
+              {8, 0},
+              {6, 0},
+              {5, 6},
+              {7, 2},
+              {0, 9},
+          }}},
+    {85, {1, {
+                 10,
+                 16,
+                 13,
+                 4,
+                 19,
+                 8,
+                 18,
+                 17,
+                 18,
+                 19,
+                 10,
+             },
+          {
+              {9, 7},
+              {9, 6},
+              {10, 4},
+              {4, 9},
+              {7, 1},
+              {0, 2},
+              {9, 2},
+              {3, 8},
+              {2, 3},
+              {5, 4},
+          }}},
+    {79, {1, {
+                 8,
+                 14,
+                 11,
+                 8,
+                 1,
+                 13,
+                 9,
+                 14,
+                 15,
+                 12,
+                 1,
+                 11,
+             },
+          {
+              {9, 1},
+              {1, 2},
+              {1, 4},
+              {5, 10},
+              {7, 8},
+              {3, 7},
+              {11, 3},
+              {11, 10},
+              {6, 8},
+              {0, 1},
+              {0, 3},
+          }}},
+    {102, {1, {
+                  15,
+                  10,
+                  18,
+                  18,
+                  3,
+                  4,
+                  18,
+                  12,
+                  6,
+                  19,
+                  9,
+                  19,
+                  10,
+              },
+           {
+               {10, 2},
+               {11, 10},
+               {6, 3},
+               {10, 8},
+               {5, 3},
+               {11, 1},
+               {9, 5},
+               {0, 4},
+               {12, 3},
+               {9, 7},
+               {11, 9},
+               {4, 12},
+           }}},
+    {93, {1, {
+                 1,
+                 7,
+                 6,
+                 18,
+                 15,
+                 2,
+                 14,
+                 15,
+                 18,
+                 8,
+                 15,
+                 1,
+                 5,
+                 6,
+             },
+          {
+              {0, 13},
+              {6, 12},
+              {0, 12},
+              {7, 8},
+              {8, 3},
+              {12, 11},
+              {12, 1},
+              {10, 12},
+              {2, 6},
+              {6, 9},
+              {12, 7},
+              {0, 4},
+              {0, 5},
+          }}},
 };
 
 const std::vector<TestCase> BONUS_TESTS = {
-  { 3, { 2, { 1, 1, 1, 2 }, { {0,3}, {1,3}, {2,3} }}},
-  { 5, { 2, { 1, 1, 1, 4 }, { {0,3}, {1,3}, {2,3} }}},
+    {3, {2, {1, 1, 1, 2}, {{0, 3}, {1, 3}, {2, 3}}}},
+    {5, {2, {1, 1, 1, 4}, {{0, 3}, {1, 3}, {2, 3}}}},
 };
 
-void test(const std::vector<TestCase>& T) {
+void test(const std::vector<TestCase> &T)
+{
   int i = 0;
-  for (auto &[s, t] : T) {
+  for (auto &[s, t] : T)
+  {
     if (s != solve(t))
-      std::cout << "Error in " << i << " (returned " << solve(t) << ")"<< std::endl;
+      std::cout << "Error in " << i << " (returned " << solve(t) << ")" << std::endl;
     i++;
   }
   std::cout << "Finished" << std::endl;
 }
 
-int main() {
+int main()
+{
   test(BASIC_TESTS);
   // test(BONUS_TESTS);
 }
 
 #endif
-
-
