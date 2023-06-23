b4c3b55e32b410035229d55ebee91bd4b566a7f4
Doli282 <DolanskyL@email.cz>
2023-06-23 13:20:16 +0200

    [ham] hw4: Detect ip version 4

--- ham/du04/kernel-monitor/monlib/src/monlib.c
+++ ham/du04/kernel-monitor/monlib/src/monlib.c
@@ -1,6 +1,9 @@
 #include "monlib.h"
 
 static int packet_processed = 0;
+static int ipv4 = 0;
+static int ipv6 = 0;
+
 int monlib_init(struct monlib_ctx *ctx)
 {
     int test_int = 10;
@@ -10,9 +13,39 @@ int monlib_init(struct monlib_ctx *ctx)
     return 0;
 }
 
+void ipv4_process(struct monlib_ctx *ctx, const void *packet, const unsigned int packet_len)
+{
+    ipv4++;
+
+}
+
+void ipv6_process(struct monlib_ctx *ctx, const void *packet, const unsigned int packet_len)
+{
+    ipv6++;
+}
+
 int monlib_process(struct monlib_ctx *ctx, const void *packet, const unsigned int packet_len)
 {
+    unsigned char * p = packet;
+
+    // total sum of processed packets
     packet_processed++;
+
+    // IP version of the packets
+    unsigned char byte = ((*p) >> 4);
+    if(byte == 4)
+    {
+        ipv4_process(ctx, packet, packet_len);
+    }
+    else //if (byte == 6)
+    {
+        ipv6_process(ctx, packet, packet_len);
+    }
+   // else
+   // { // unknown ip version
+   //     return 1;
+   // }
+
     return 0;
 }
 
@@ -20,6 +53,8 @@ struct monlib_stats monlib_get_stats(struct monlib_ctx *ctx)
 {
     struct monlib_stats stats;
     stats.packets = packet_processed;
+    stats.ipv4_packets = ipv4;
+    stats.ipv6_packets = ipv6;
     return stats;
 }
 
