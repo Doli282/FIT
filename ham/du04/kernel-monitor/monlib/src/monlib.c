ad0f7b39a1f28a455cdea3592fa907b7a69ff8ab
Doli282 <DolanskyL@email.cz>
2023-06-23 14:21:37 +0200

    [ham] hw4: Detect TCP and UDP

--- ham/du04/kernel-monitor/monlib/src/monlib.c
+++ ham/du04/kernel-monitor/monlib/src/monlib.c
@@ -1,8 +1,26 @@
 #include "monlib.h"
 
-static int packet_processed = 0;
-static int ipv4 = 0;
-static int ipv6 = 0;
+static unsigned int packet_processed = 0;   /* Number of processed packets */
+static unsigned int ipv4 = 0;               /* Number of IPv4 processed packets */
+static unsigned int ipv6 = 0;               /* Number of IPv6 processed packets */
+static unsigned int tcp_packets = 0;        /* Number of TCP processed packets */
+static unsigned int tcp_syns = 0;           /* Number of TCP packets containing syn flag */
+static unsigned int packets = 0;            /* Number of processed packets */
+static unsigned int ipv4_packets = 0;       /* Number of IPv4 processed packets */
+static unsigned int ipv6_packets = 0;       /* Number of IPv6 processed packets */
+static unsigned int tcp_packets = 0;        /* Number of TCP processed packets */
+static unsigned int tcp_syns = 0;           /* Number of TCP packets containing syn flag */
+static unsigned int tcp_fins = 0;           /* Number of TCP packets containing fin flag*/
+static unsigned int udp_packets = 0;        /* Number of UDP processed packets */
+static unsigned int max_src_port = 0;       /* Number of Maximal seen source port(TCP,UDP) */
+static unsigned int min_dst_port = 65536;       /* Number of Minimal seen destination port(TCP,UDP) */
+
+static unsigned int avr_byte_len = 0;       /* Average byte length of the packets */
+
+static unsigned int flows = 0;              /* Number of processed flows */
+static unsigned int avr_flow_p_len = 0;     /* Average number of packets inside flow */
+static unsigned int avr_flow_b_len = 0;     /* Average number of bytes inside flow  */
+
 
 int monlib_init(struct monlib_ctx *ctx)
 {
@@ -13,38 +31,106 @@ int monlib_init(struct monlib_ctx *ctx)
     return 0;
 }
 
-void ipv4_process(struct monlib_ctx *ctx, const void *packet, const unsigned int packet_len)
+unsigned char ipv4_process(const unsigned char *packet, const unsigned char * start_of_L4)
 {
+    // add one to detected ipv4 packets
     ipv4++;
 
+    // return L4 protocol
+    return packet[9];    
+    
 }
 
-void ipv6_process(struct monlib_ctx *ctx, const void *packet, const unsigned int packet_len)
+unsigned char ipv6_process(const unsigned char *packet, const unsigned char * start_of_L4)
 {
+    // add one to detected ipv6 packets
     ipv6++;
+
+    // check L4 Protocol
+    start_of_L4 = packet + 40;
+    unsigned char byte = *(packet += 6);
+    // next ipv6 header
+    while (byte == 0 || byte == 43 || byte == 44 || byte == 51 || byte == 60)
+           // || byte == 50 || byte == 135 || byte == 139 || byte == 140) // <- will be considered invalid
+    {
+        // Hop-by-Hop Options || Routing || Destination Options
+        if(byte == 0 || byte == 43 || 60)
+        {
+            // check next header and skip this header
+            byte == start_of_L4[0];
+            start_of_L4 += start_of_L4[1] + 1;
+        }
+        // Fragment
+        else if(byte == 44)
+        {
+            // check next header and skip this header
+            byte == start_of_L4[0];
+            start_of_L4 += 8;
+        }
+        // Authentication Header
+        else if(byte == 51)
+        {
+            // check next header and skip this header
+            byte == start_of_L4[0];
+            start_of_L4 += (start_of_L4[1] + 2) * 4;
+        }
+    }
+
+    // return L4 protocol
+    return byte;
+}
+
+unsigned char udp(const unsigned char * packet)
+{
+    // add one more to detected udp packets
+    udp_packets++;
+    return 0;
+}
+
+unsigned char tcp(const unsigned char * packet)
+{
+    // add one more to tedected tcp packets
+    tcp_packets++;
+    return 0;
 }
 
 int monlib_process(struct monlib_ctx *ctx, const void *packet, const unsigned int packet_len)
 {
-    unsigned char * p = packet;
+    // skip the ethernet header
+    packet += 14;
+    const unsigned char * p = packet;
 
     // total sum of processed packets
     packet_processed++;
 
-    // IP version of the packets
+    // check L3 Protocol
     unsigned char byte = ((*p) >> 4);
     if(byte == 4)
     {
-        ipv4_process(ctx, packet, packet_len);
+        byte = ipv4_process(packet, p);
     }
-    else //if (byte == 6)
+    else if (byte == 6)
     {
-        ipv6_process(ctx, packet, packet_len);
+        byte = ipv6_process(packet, p);
     }
-   // else
-   // { // unknown ip version
-   //     return 1;
-   // }
+    else
+    { // unknown ip version
+        return 1;
+    }
+
+    // check L4 Protocol
+    if(byte == 6) // TCP
+    {
+        tcp(p);
+    }
+    else if (byte == 17) // UDP
+    {
+        udp(p);
+    }
+    else // unknown protocol
+    {
+        return 1;
+    }
 
     return 0;
 }
